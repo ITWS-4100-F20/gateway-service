b3c5243081b7465d48e6338aa787ed20f3af8731
Noah Deahl <Noah.Deahl@ibm.com>
2020-09-24 15:00:46 -0400

    Initial flask app files and swagger doc

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
+
+app = Flask(__name__)
+
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Oversale Solicitation API"
+    },
+)
+
+# Register blueprint at URL
+# (URL must match the one given to factory function above)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+app.run()
