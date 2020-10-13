de16320dc58e7d56f6d8bfaa61d2ac0efcb6036f
Noah Deahl <Noah.Deahl@ibm.com>
2020-10-13 15:59:49 -0400

    Update swagger-server layout and documentation

---
+++ swagger_server/models/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.create_user import CreateUser
+from swagger_server.models.error import Error
+from swagger_server.models.success import Success
+from swagger_server.models.user import User
