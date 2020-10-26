3b709d4e224b979cd6479e6a6a74aae037a16e8c
Noah Deahl <Noah.Deahl@ibm.com>
2020-10-25 20:55:11 -0400

    Change to implicit OAuth flow for swagger to fix CORS issue

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,6 +1,7 @@
 import connexion
 import os
 import msal
+from flask_cors import CORS
 from flask_session import Session 
 from swagger_server import encoder
 from swagger_server import config
@@ -9,6 +10,7 @@ app=connexion.FlaskApp(
         __name__, 
         specification_dir='./swagger'
     )
+CORS(app.app, resources={r"/*": {"origins": "*"}})
 
 # @app.route("/login")
 # def login():
@@ -85,10 +87,10 @@ def _get_token_from_cache(scope=None):
         return result    
 
 flask_app = app.app
+flask_app.config['CORS_HEADERS'] = 'Content-Type'
 flask_app.config.from_object(config)
 Session(flask_app)
 app.add_api('swagger.yaml', strict_validation=True, options={ "swagger_ui_config": {"oauth2RedirectUrl": "http://localhost:3030/api/ui/oauth2-redirect.html"}} )
 flask_app.json_encoder = encoder.JSONEncoder
-
 if __name__ == '__main__':
     app.run(port=3030)
