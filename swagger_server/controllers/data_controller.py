3b9ff440a00622f56692f614725b0597966b25e6
Noah Deahl <Noah.Deahl@ibm.com>
2020-10-28 17:52:31 -0400

    Schema controller addition

--- swagger_server/controllers/data_controller.py
+++ swagger_server/controllers/data_controller.py
@@ -1,27 +1,37 @@
 import connexion
 import six
 
+from swagger_server.models.data_row import DataRow  # noqa: E501
+from swagger_server.models.data_schema import DataSchema  # noqa: E501
 from swagger_server import util
 
 
-def delete_data():  # noqa: E501
+def delete_data(id):  # noqa: E501
     """Delete data of scheme type
 
      # noqa: E501
 
+    :param id: id of schema
+    :type id: 
 
-    :rtype: None
+    :rtype: int
     """
     return 'do some magic!'
 
 
-def get_data():  # noqa: E501
+def get_data(id, amount=None, filters=None):  # noqa: E501
     """Get data of scheme type
 
      # noqa: E501
 
+    :param id: id of schema
+    :type id: 
+    :param amount: amount of data
+    :type amount: int
+    :param filters: column specific filters (..:columnname,value)
+    :type filters: List[str]
 
-    :rtype: None
+    :rtype: List[DataRow]
     """
     return 'do some magic!'
 
@@ -32,28 +42,36 @@ def get_data_schemes():  # noqa: E501
      # noqa: E501
 
 
-    :rtype: None
+    :rtype: List[DataSchema]
     """
     return 'do some magic!'
 
 
-def post_add_data():  # noqa: E501
+def post_add_data(id, data):  # noqa: E501
     """Add data of scheme type
 
      # noqa: E501
 
+    :param id: id of schema
+    :type id: 
+    :param data: data to be added
+    :type data: str
 
     :rtype: None
     """
     return 'do some magic!'
 
 
-def put_add_scheme():  # noqa: E501
+def put_add_scheme(schema):  # noqa: E501
     """Add data scheme
 
      # noqa: E501
 
+    :param schema: schema json
+    :type schema: dict | bytes
 
     :rtype: None
     """
+    if connexion.request.is_json:
+        schema = DataRow.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
---
+++ swagger_server/controllers/scenario_controller.py
@@ -0,0 +1,26 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def get_scenario():  # noqa: E501
+    """Lists scenarios with no id. With id it returns data for scenario
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def post_scenario():  # noqa: E501
+    """Creates new scenario
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
